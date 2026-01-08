f37747aab9aadcd11cb234b98aeb9eac49c84b0d
rolladharani <rolladharani77@gmail.com>
2026-01-08 11:19:37 +0530

    changes

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
 const express = require('express')
-const app = express()
-app.listen(8000,()=>{
-    console.log("Server started at 8000");
+const app = express();
+const mongoose = require('mongoose')
+
+mongoose.connect("mongodb+srv://umanandeswarikunisetti17_db_user:UmaPavan_1617@cluster0.5smw3lb.mongodb.net/")
+
+.then(() => console.log("DB Connected"))
+.catch(() => console.log(error))
+
+// const FirstRoute = require("./src/Routes/FirstRoute")
+// app.use("/", FirstRoute);
+
+app.listen(7000, () =>{
+    console.log("Server started at port 7000");
 })
