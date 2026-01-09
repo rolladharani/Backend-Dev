ad92e0a2cfb915702d34c2297214f1ffc14a305f
rolladharani <rolladharani77@gmail.com>
2026-01-09 23:33:12 +0530

    Changes

---
+++ src/Controllers/FirstController.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const Credentials = require("../Models/Credentials.js")
+
+const TestingAPI = (req,res) =>{
+    console.log("Responding....");
+}
+
+exports.x = TestingAPI;
