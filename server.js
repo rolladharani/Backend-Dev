7629f992df5708dfe1e008774b0da8f9a93319f7
rolladharani <rolladharani77@gmail.com>
2026-01-27 21:47:22 +0530

    changes

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express')
 const app = express();
 const mongoose = require('mongoose')
+const path = require('path')
+const bcrypt = require('bcrypt')
+const cron=require('node-cron')
 
-mongoose.connect("mongodb+srv://umanandeswarikunisetti17_db_user:UmaPavan_1617@cluster0.5smw3lb.mongodb.net/")
-
+mongoose.connect("mongodb+srv://rolladharani77_db_user:Dharani2504@cluster0.xjaosgi.mongodb.net/")
 .then(() => console.log("DB Connected"))
-.catch(() => console.log(error))
+.catch((err) => console.log(err))
 
-// const FirstRoute = require("./src/Routes/FirstRoute")
-// app.use("/", FirstRoute);
+const FirstRoute = require("./src/Routes/FirstRoutes")
+app.use(express.json());
 
+app.use(express.urlencoded({ extended: true }));
+app.use("/", FirstRoute);
+
+cron.schedule('*/2 * * * * *', () => {
+    console.log('running a task every 2 seconds');
+  });
+
+// app.use(express.static(path.join(__dirname,"uploads")))
 app.listen(7000, () =>{
     console.log("Server started at port 7000");
 })
